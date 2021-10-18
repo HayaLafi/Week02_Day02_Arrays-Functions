0486392bb81dc160981e5549ead13626afa41342
HayaLafi <11haya.hh@gmail.com>
2021-10-18 15:30:53 +0300

    hh

---
+++ array.js
@@ -0,0 +1,68 @@
+//solve the 1 question
+let hi=[4,2,5,6,8,9,3]
+for (let i=0;i<hi.length;i++){
+    if (hi[i]%2 !=0){
+    hi[i]=hi[i]*2;
+}
+}
+console.log(marks);
+  
+
+////solve the 2 question
+let hi=[3,4,5,6];
+for (let i=0;i<hi.length;i++){
+    if (hi[i]%2 ==0){
+    hi[i]=hi[i]*2;
+}
+}
+console.log(matrix);
+//solve the 3 question
+let  arraycolor = ["yellow","red","green"];
+ console.log(arraycolor);
+ for(let i =0;i<arraycolor.length;i++){
+console.log("my #" + (i+1) +" choice is "+arraycolor[i] );
+ }
+ //solve the 4 question
+ let code = [2,3,4];
+console.log(code);
+code.unshift(1);
+code.unshift(0);
+
+code.push(3);
+code.push(4);
+code.push(5);
+console.log(member);
+
+//solve the 5 quetion 
+
+
+let Array= [2,4,6,8,10];
+let num = 1;
+let a =" ";
+for(let i =0;i<Array.length;i++){
+if (num ==Array[i]){
+
+    a ="true";
+     break;}
+   else {t="zero";}}
+console.log(a);
+
+// solve the 6 quetion
+
+let h = [1,3,5,7,9];
+console.log(h);
+let hnum = 2;
+
+for(let i =0;i<h.length;i++){
+
+
+for(let j =i+1;j<h.length-1;j++){ 
+
+if (hnum==(number[i]+number[j]))
+
+
+{ console.log(number[i] , number[j]);
+}
+}}
+
+
